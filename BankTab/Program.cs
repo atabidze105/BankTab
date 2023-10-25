f5741864f077452fbbf718ef2ad71be0570b8e5c
atabidze105 <114877662+atabidze105@users.noreply.github.com>
2023-10-25 11:12:55 +0300

    Add files via upload

---
+++ BankTab/Program.cs
@@ -0,0 +1,37 @@
+﻿using BankTab;
+class Program
+{
+    static void Main()
+    {
+        void accountChoshing(Bank[] banks, int quantity) //Метод для выбора из массива объекта для применения метода
+        {
+            Console.WriteLine($"Необходимо выбрать счет чтобы продолжить. Введите один из доступных номеров счетов:\n\nот 1 до {quantity}:\n");
+            int nom = Convert.ToInt32(Console.ReadLine())-1; //Выбор индекса элемента массива
+            banks[nom].callCenter(banks);
+        }
+        
+        Console.WriteLine("Чтобы начать работу, необходимо задать количество счетов.\nВведите количество аккаунтов, которое хотите создать:");
+        int quantityOfAccounts = Convert.ToInt32(Console.ReadLine());
+        Bank[] accounts = new Bank[quantityOfAccounts];
+        for (int i = 0; i < accounts.Length; i++) //Цикл для создания элементов массива
+        {
+            accounts[i] = new Bank();
+        }
+
+        accountChoshing(accounts, quantityOfAccounts); //Первое обращение к методу        
+        
+        Console.WriteLine("\nЧтобы вернуться к выбору аккаунта, нажмите \"Enter\"");
+        string thisAccount = ""; //Переменная для поддержания работы следующего цикла
+        thisAccount = Console.ReadLine();
+
+        if (thisAccount == "")
+        {
+            do
+            {
+                accountChoshing(accounts, quantityOfAccounts);
+                Console.WriteLine("Чтобы вернуться к выбору аккаунта, нажмите \"Enter\"");
+                thisAccount = Console.ReadLine();
+            } while (thisAccount == ""); //Метод работает пока строка пуста
+        }
+    }
+}
